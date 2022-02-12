80afd46a51f61700c99204a14b5aa09d970f597e
Anais Augu <augu.anais@gmail.com>
2022-02-12 14:18:52 +0100

    lists controller ok

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,34 @@
+class ListsController < ApplicationController
+  # before_action :find_list, only: [:show]
+
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  # def find_list
+  #   @list = List.find(param[:id])
+  # end
+end
