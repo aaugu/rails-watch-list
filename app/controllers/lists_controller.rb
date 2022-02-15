3e794136f038b1104a0be1457aeeef535d374dcd
Anais Augu <augu.anais@gmail.com>
2022-02-15 17:00:32 +0100

    front ok, ready for next exercise

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,5 @@
 class ListsController < ApplicationController
+  before_action :find_list, only: [:show, :destroy]
 
   def index
     @lists = List.all
@@ -21,7 +22,16 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
 
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
   private
+
+  def find_list
+    @list = List.find(params[:id])
+  end
 
   def list_params
     params.require(:list).permit(:name)
